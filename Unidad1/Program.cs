884601d6ec366925a2fb4f86640764122c25ae4a
FabianDaSilva <dasilvafabian.j@gmail.com>
2026-03-29 22:25:10 -0300

    Unidad 1; 4-8

--- Unidad1/Program.cs
+++ Unidad1/Program.cs
@@ -11,6 +11,21 @@ switch (opcion)
     case "3":
         Ejercicio3(); break;
 
+    case "4":
+        Ejercicio4(); break;
+
+    case "5":
+        Ejercicio5(); break;
+
+    case "6":
+        Ejercicio6(); break;
+
+    case "7":
+        Ejercicio7(); break;
+
+    case "8":
+        Ejercicio8(); break;
+
     default:
         Console.WriteLine("No hay mas ejercicios para ejecutar");
         break;
@@ -39,6 +54,7 @@ static void Ejercicio2()
 }
 
 static void Ejercicio3()
+
 {
     int primero, segundo;
 
@@ -60,6 +76,109 @@ static void Ejercicio3()
 
     System.Console.WriteLine($"El promedio es de: {promedio}");
 
+}
+
+static void Ejercicio4()
+{
+    int horas, minutos, segundos, total;
+
+    do
+    {
+        System.Console.WriteLine("Por favor ingresa las horas (0-23): ");
+    } while (!int.TryParse(Console.ReadLine(), out horas) || horas < 0 || horas > 23);
+
+    do
+    {
+        System.Console.WriteLine("Ingresa los minutos (0-59):");
+    } while (!int.TryParse(Console.ReadLine(), out minutos) || minutos < 0 || minutos > 59);
+
+    do
+    {
+        System.Console.WriteLine("Ingresa los segundos (0-60): ");
+    } while (!int.TryParse(Console.ReadLine(), out segundos) || segundos < 0 || segundos > 60);
+
+    total = horas * 3600 + minutos * 60 + segundos;
+
+    System.Console.WriteLine($"El tiempo ingresado fue: {total}");
+}
+
+static void Ejercicio5()
+{
+    string variable1, variable2, aux = "";
+
+    System.Console.WriteLine("Ingresa el primer valor");
+    variable1 = Console.ReadLine();
+    System.Console.WriteLine("Ingresa el segundo valor");
+    variable2 = Console.ReadLine();
+    System.Console.WriteLine($"Checkpoint Variable1: {variable1} Variable2: {variable2}");
+
+    aux = variable1;
+    variable1 = variable2;
+    variable2 = aux;
+
+    System.Console.WriteLine($"Checkpoint intercambi de valores. Variable1: {variable1} Variable2: {variable2}");
 
 
+}
+
+static void Ejercicio6()
+{
+    int ladoA, ladoB, ladoC;
+
+    System.Console.WriteLine("Ingresa el primer lado: ");
+    int.TryParse(Console.ReadLine(), out ladoA);
+
+    System.Console.WriteLine("Ingresa el segundo lado: ");
+    int.TryParse(Console.ReadLine(), out ladoB);
+
+    System.Console.WriteLine("Ingresa el tercer lado: ");
+    int.TryParse(Console.ReadLine(), out ladoC);
+
+    if (ladoA == ladoB && ladoA == ladoC)
+    {
+        Console.WriteLine("Es un Equilátero");
+    }
+    else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+    {
+        Console.WriteLine("Es un Isósceles");
+    }
+    else
+    {
+        Console.WriteLine("Es Escaleno");
+    }
+
+
+}
+
+static void Ejercicio7()
+{
+    System.Console.WriteLine("");
+    for (int i = 0; i < 99; i++)
+    {
+        if (i % 10 == 0)
+        {
+            System.Console.WriteLine($"Valor {i}");
+        }
+    }
+}
+
+static void Ejercicio8()
+{
+    System.Console.WriteLine("==== Ejercicio 8 ====");
+
+    int monto = 0, total = 0;
+    string continuar = "";
+
+    do
+    {
+        System.Console.WriteLine("Por favor ingresar el monto: ");
+        int.TryParse(Console.ReadLine(), out monto);
+        total += monto;
+
+        System.Console.WriteLine("Desea continuar? S/N: ");
+        continuar = System.Console.ReadLine();
+
+    } while (continuar.ToUpper() == "S");
+
+    System.Console.WriteLine($"Facturacion final es de: {total}");
 }
